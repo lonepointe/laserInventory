46356efd71e73f80c6a03375c0c80c03835850a7
lonepointe <psycholabs@gmail.com>
2025-05-13 14:07:43 -0400

    adding python elements early

--- laserInventory.py
+++ laserInventory.py
@@ -1,1 +1,19 @@
+import sqlite3
+
+conn = sqlite3.connect('laser_inventory.db')
+print("Connected to database.")
+conn.close
+
 print("hell yes, initial commit!")
+
+
+def add_stock():
+    name = input("What item? ")
+    quantity = input("How many? ")
+    unit = input("What unit? (sheets, pcs, etc): ")
+    location = input("Where is it stored? ")
+
+    print(f"\nYou added: {quantity} {unit} of {name} at {location}.")
+
+
+add_stock()
